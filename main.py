9c27d1c370c44c3299f7f9ac186bbef5799b62d0
Nithi <nithish@dreaminfinity.in>
2025-05-31 23:22:53 +0530

    change

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ def add(a, b):
     Adds two numbers together. 
     """
     return a + b
+
+def subtrEact(a, b):
+    """
+    Subtracts the second number from the first.
+    """
+    return a - b
